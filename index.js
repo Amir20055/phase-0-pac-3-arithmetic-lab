0e5513fe9ec56192bd8605e2448e1bd832c69d14
amir hassan <amir0486@yahoo.com>
2022-03-19 23:55:44 -0700

    done with it

---
+++ index.js
@@ -0,0 +1,67 @@
+function add() {
+    console.log(add=`+`);
+}
+function subtract() {
+    console.log(subtract=`-`);
+}
+function multiply() {
+    console.log(multiply= '*');
+}
+function divide() {
+    console.log(divide= `/`);
+}
+function add(a , b) {
+  return (a+b); 
+}
+function subtract(a , b) {
+    return (a-b);
+}
+function multiply(a , b) {
+    return (a*b);
+}
+function divide(a , b) {
+    return (a/b);
+}
+function increment(n) {
+    return (n+1);
+}
+function decrement(n) {
+    return (n-1);
+}
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+    console.log(parseFloat("2.222"));
+    console.log(parseFloat('sldjdjfk'));
+    return parseFloat(n);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
